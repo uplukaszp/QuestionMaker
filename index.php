39ec6d0939ce62ff5c5006ffc1a2bce14ed63bdf
Lukasz <uplukaszp1@gmail.com>
2018-01-12 15:32:21 +0100

    Table name update

--- index.php
+++ index.php
@@ -8,12 +8,14 @@
 
 <ul>
 <?php
+
 $dba_name = getenv("db_name");
 $mysql_username = getenv("login");
 $mysql_password = getenv("pass");
 $server_name = getenv("serv_name");
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $dba_name); 
-$mysql_qry = "select * from category"; 
+mysqli_set_charset($conn,"utf8");
+$mysql_qry = "select * from categories"; 
 $result = mysqli_query( $conn,$mysql_qry);
 
 while($row = mysqli_fetch_assoc($result)) {
--- insert_cat.php
+++ insert_cat.php
@@ -3,7 +3,7 @@ require "index.php";
 if(isset($_POST['kat']))
 {
 	$kat=$_POST['kat'];
-	$mysql_qry = "insert into category values('','$kat')"; 
+	$mysql_qry = "insert into categories values('','$kat')"; 
 	$result = mysqli_query( $conn,$mysql_qry);
 	header("Location: index.php");
 }
